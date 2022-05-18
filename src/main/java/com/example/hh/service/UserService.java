9dd3b3801c0ca14fbf8780b7e49d26b9161eec4d
오어진 <dhdjwlsdlaehk123@naver.com>
2022-05-18 11:01:31 +0900

    fix

--- src/main/java/com/example/hh/service/UserService.java
+++ src/main/java/com/example/hh/service/UserService.java
@@ -107,7 +107,7 @@ public class UserService {
             GetProfileResponse getProfileResponse = GetProfileResponse.builder()
                     .email(user.getEmail())
                     .imageUrl("")
-                    .description("asdfsd")
+                    .description("")
                     .userName(user.getUserName())
                     .build();
 
